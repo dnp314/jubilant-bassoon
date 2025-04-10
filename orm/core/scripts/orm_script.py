81f879974982864a7ac46ce95105d0f5cac9e1db
Divyesh Parikh <dcparikh360@gmail.com>
2025-04-10 15:51:07 +0530

    Querying and Creating Records

---
+++ orm/core/scripts/orm_script.py
@@ -0,0 +1,9 @@
+from core.models import Restaurant, Rating, Sale
+from django.utils import timezone
+from django.db import connection
+from django.contrib.auth.models import User
+from pprint import pprint
+
+def run():
+  pass
+  
