604c2d48d7d7903f419450324fdd570291de56ad
Divyesh Parikh <dcparikh360@gmail.com>
2025-04-11 23:12:12 +0530

    Added Validators

--- orm/core/models.py
+++ orm/core/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.core.exceptions import ValidationError
 # Create your models here.
+
+def valid_resto_name_begin_with_a(value):
+  if not value.startswith('a'):
+    raise ValidationError("Yo whatsupp")
+
 
 class Restaurant(models.Model):
   class TypeChoices(models.TextChoices):
@@ -11,8 +18,12 @@ class Restaurant(models.Model):
   name = models.CharField(max_length=20)
   website = models.URLField(default='')
   date_opened = models.DateField()
-  latitude = models.FloatField()
-  longitude = models.FloatField()
+  latitude = models.FloatField(
+    validators=[MinValueValidator(-90), MaxValueValidator(90)]
+  )
+  longitude = models.FloatField(
+    validators=[MinValueValidator(-180), MaxValueValidator(180)]
+  )
   restaurant_type = models.CharField(max_length=2, choices=TypeChoices.choices)
   
   def __str__(self):
@@ -21,7 +32,9 @@ class Restaurant(models.Model):
 class Rating(models.Model):
   user = models.ForeignKey(User, on_delete=models.CASCADE)
   restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name="ratings")
-  rating = models.PositiveSmallIntegerField()
+  rating = models.PositiveSmallIntegerField(
+    validators=[MinValueValidator(1), MaxValueValidator(5)]
+  )
   
   def __str__(self):
     return f"Rating: {self.rating}"
